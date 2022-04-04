d18af7114e1161e22f2242f5328f90985b291263
MinhDuc3011 <leminhduc30112001@gmail.com>
2022-04-04 16:08:21 +0700

    update commit

---
+++ FormValidate1/validator.js
@@ -0,0 +1,153 @@
+// Đối tượng Validator
+function Validator(options) {
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+
+    var selectorRules = {};
+    // Hàm thực hiện validate
+    function validate(inputElement, rule) {
+        var errorElement = getParent(inputElement,options.formGroupSelector).querySelector(options.errorSelector);
+        var errorMessage;
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rule và kiểm tra
+        // Nếu có lỗi thì dừng việc kiểm tra
+        for (var i = 0; i < rules.length; ++i) {
+            errorMessage = rules[i](inputElement.value); 
+            if (errorMessage) break;
+        } 
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+        }
+
+        return !errorMessage;
+
+    }
+
+    // Lấy element của form cần validate
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        // Khi submit form
+        formElement.onsubmit = function(e) {
+            e.preventDefault();
+
+            var isFormValid = true;
+
+            // Lặp qua từng rule và validate
+            options.rules.forEach(function (rule) {
+            var inputElement = formElement.querySelector(rule.selector);
+            var isValid = validate(inputElement, rule);
+            if (!isValid) {
+                isFormValid = false;
+            }
+            });
+            
+            
+            
+            if (isFormValid) {
+                // Trường hợp submit với javascript
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]');
+            
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        values[input.name] = input.value
+                        return values;
+                    }, {});
+ 
+                    options.onSubmit(formValues);
+                }
+                // Trường hợp submit với hàm mặc định
+                else {
+                    formElement.submit();
+                }
+            } 
+        }
+
+
+        // Xử lý lặp qua mỗi rule (lắng nghe sự kiện blur, innput ...)
+        options.rules.forEach(function (rule) {
+
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            }
+            else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
+            var inputElements = formElement.querySelectorAll(rule.selector);
+
+            Array.from(inputElements).forEach(function (inputElement) {
+                // Xử lý trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);                  
+                }
+
+                // Xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function () {
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector('.form-message');                  
+                    errorElement.innerText = '';
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+                }
+            });
+           
+        });
+    }
+}
+
+
+
+// Định nghĩa các rules
+// Nguyên tắc các rules:
+// 1. Khi có lỗi thả ra message lỗi
+// 2. Khi hợp lệ không trả ra gì (undefined)
+Validator.isRequired = function(selector, message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value.trim() ? undefined : message || "Vui lòng nhập trường này"
+        }
+    }
+}
+
+Validator.isEmail = function(selector, message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined :  message || "Trường này phải là email"
+        }
+    }
+}
+
+Validator.minLength = function(selector, min, message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value.length >= min ? undefined :  message || `Vui lòng nhập tối thiểu ${min} kí tự`
+        }
+    }
+}
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue() ? undefined : message || "Giá trị nhập vào không chính xác"
+        }
+    }
+}
+
--- [ISD 2022] Final Project/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$con = mysqli_connect('localhost', 'root', 'manage_account') or die('connection failed');
-$my_email = "1901040060@S.hanu.edu.vn";
-$link_reset = "http://localhost:3000/login.php";
-?>
